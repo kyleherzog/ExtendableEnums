65d392cc3761a9aeed9aa3b90a26e8b812f34b7a
Kyle Herzog <holler@kyleherzog.com>
2019-01-18 17:43:50 -0600

    addressed some analyzer warnings

--- ExtendableEnums.OData.TestHost/Controllers/SampleBooksController.cs
+++ ExtendableEnums.OData.TestHost/Controllers/SampleBooksController.cs
@@ -6,10 +6,12 @@ namespace ExtendableEnums.OData.TestHost.Controllers
 {
     public class SampleBooksController : ODataController
     {
+        private readonly IList<SampleBook> books = DataContext.Books;
+
         [EnableQuery]
         public IEnumerable<SampleBook> Get()
         {
-            return DataContext.Books;
+            return books;
         }
     }
 }
--- ExtendableEnums.UnitTests/GlobalSuppressions.cs
+++ ExtendableEnums.UnitTests/GlobalSuppressions.cs
@@ -4,3 +4,4 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1036:Overload operator Equals and comparison operators when implementing System.IComparable", Justification = "We only compare equality based on base class Value property.", Scope = "type", Target = "~T:ExtendableEnums.UnitTests.Models.SampleStatus")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1067:Override Object.Equals(object) when implementing IEquatable<T>", Justification = "We only compare equality based on base class Value property", Scope = "type", Target = "~T:ExtendableEnums.UnitTests.ExpandableEnumerationTests.ConstructorShould.NullValueEnum")]
