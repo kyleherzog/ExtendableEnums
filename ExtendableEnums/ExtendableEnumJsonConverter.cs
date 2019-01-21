18546d7dcb494cab233985b9723d53696bdf987f
Kyle Herzog <holler@kyleherzog.com>
2019-01-21 15:46:27 -0600

    Allowing deserialization from object with value property

--- ExtendableEnums/ExtendableEnumJsonConverter.cs
+++ ExtendableEnums/ExtendableEnumJsonConverter.cs
@@ -3,13 +3,24 @@ using System.Collections.Concurrent;
 using System.Globalization;
 using System.Reflection;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ExtendableEnums
 {
     public class ExtendableEnumJsonConverter : JsonConverter
     {
         private static readonly ConcurrentDictionary<Type, MethodInfo> parseValueMethodCache = new ConcurrentDictionary<Type, MethodInfo>();
+
+        public static object GetDefault(Type t)
+        {
+            if (t.IsValueType && Nullable.GetUnderlyingType(t) == null)
+            {
+                return Activator.CreateInstance(t);
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         public override bool CanConvert(Type objectType)
         {
@@ -19,20 +30,27 @@ namespace ExtendableEnums
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var valueType = GetTypeOfValueParameter(objectType);
-
             var method = GetParseValueMethod(objectType);
 
             var rawValue = reader.Value;
             if (rawValue == null)
             {
-                reader.Read(); // "value"
-                reader.Read(); // the actual value
-                rawValue = reader.Value;
+                var dynamicObject = serializer.Deserialize<dynamic>(reader);
+                rawValue = dynamicObject.value?.Value;
             }
 
-            var value = Convert.ChangeType(rawValue, valueType, CultureInfo.InvariantCulture);
-            var result = method.Invoke(null, new object[] { value });
-            return result;
+            if (rawValue == null)
+            {
+                var value = GetDefault(valueType);
+                var result = method.Invoke(null, new object[] { value });
+                return result;
+            }
+            else
+            {
+                var value = Convert.ChangeType(rawValue, valueType, CultureInfo.InvariantCulture);
+                var result = method.Invoke(null, new object[] { value });
+                return result;
+            }
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
