94dff246c257e13ddaa8a020a6ef4ec3eeef12dd
Kyle Herzog <holler@kyleherzog.com>
2019-02-03 17:49:58 -0600

    Added type converter to convert from string

---
+++ ExtendableEnums.Testing.Models/SampleStatusByString.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using ExtendableEnums;
+
+namespace ExtendableEnums.Testing.Models
+{
+    public class SampleStatusByString : ExtendableEnumBase<SampleStatusByString, string>
+    {
+        public static readonly SampleStatusByString Unknown = new SampleStatusByString("A", nameof(Unknown), "???");
+        public static readonly SampleStatusByString Active = new SampleStatusByString("B", nameof(Active), "ACT");
+        public static readonly SampleStatusByString Deleted = new SampleStatusByString("C", nameof(Deleted), "DEL");
+        public static readonly SampleStatusByString Discontinued = new SampleStatusByString("D", nameof(Discontinued), "DIS");
+        public static readonly SampleStatusByString Inactive = new SampleStatusByString("E", nameof(Inactive), "INA");
+
+        private SampleStatusByString(string value, string displayName, string code)
+            : base(value, displayName)
+        {
+            Code = code;
+        }
+
+        [NotMapped]
+        public string Code { get; }
+    }
+}
--- ExtendableEnums.UnitTests/ExtendableEnumTests/SerializeShould.cs
+++ ExtendableEnums.UnitTests/ExtendableEnumTests/SerializeShould.cs
@@ -1,4 +1,6 @@
-﻿using ExtendableEnums.Testing.Models;
+﻿using System;
+using System.Collections.Generic;
+using ExtendableEnums.Testing.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 
@@ -42,6 +44,18 @@ namespace ExtendableEnums.UnitTests.ExpandableEnumerationTests
             var serialized = JsonConvert.SerializeObject(status);
 
             Assert.AreEqual($"{status.Value}", serialized);
+        }
+
+        [TestMethod]
+        public void DeserializeFromSerializedDictionaryGivenExtendedEnumIsKey()
+        {
+            var dictionary = new Dictionary<SampleStatus, string>();
+            var keyStatus = SampleStatus.Discontinued;
+            dictionary.Add(keyStatus, "test value");
+            var serialized = JsonConvert.SerializeObject(dictionary);
+            Console.WriteLine(serialized);
+            var deserialized = JsonConvert.DeserializeObject<Dictionary<SampleStatus, string>>(serialized);
+            Assert.AreEqual(dictionary[keyStatus], deserialized[keyStatus]);
         }
     }
 }
