cc9fdb22740dae36e0abdc3986a4b894a9f6a29b
Kyle Herzog <holler@kyleherzog.com>
2019-07-26 14:55:24 -0500

    Added IsExtendableEnum extension method

--- ExtendableEnums.Microsoft.AspNetCore/ExtendableEnumModelBinderProvider.cs
+++ ExtendableEnums.Microsoft.AspNetCore/ExtendableEnumModelBinderProvider.cs
@@ -3,28 +3,16 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ExtendableEnums.Microsoft.AspNetCore
 {
+    /// <summary>
+    /// The <see cref="IModelBinderProvider"/> that provides model binding for ExtendableEnums.
+    /// </summary>
     public class ExtendableEnumModelBinderProvider : IModelBinderProvider
     {
-        public static bool IsTypeDerivedFromGenericType(Type typeToCheck, Type genericType)
-        {
-            if (typeToCheck == typeof(object))
-            {
-                return false;
-            }
-            else if (typeToCheck == null)
-            {
-                return false;
-            }
-            else if (typeToCheck.IsGenericType && typeToCheck.GetGenericTypeDefinition() == genericType)
-            {
-                return true;
-            }
-            else
-            {
-                return IsTypeDerivedFromGenericType(typeToCheck.BaseType, genericType);
-            }
-        }
-
+        /// <summary>
+        /// Creates a IModelBinder based on ModelBinderProviderContext.
+        /// </summary>
+        /// <param name="context">The current <see cref="ModelBinderProviderContext"/>.</param>
+        /// <returns>An <see cref="ExtendableEnumBinder"/> if the model type derives from <see cref="ExtendableEnumBase{TEnumeration, TValue}"/>, otherwise null.</returns>
         public IModelBinder GetBinder(ModelBinderProviderContext context)
         {
             if (context == null)
@@ -32,7 +20,7 @@ namespace ExtendableEnums.Microsoft.AspNetCore
                 throw new ArgumentNullException(nameof(context));
             }
 
-            if (IsTypeDerivedFromGenericType(context.Metadata.ModelType, typeof(ExtendableEnumBase<,>)))
+            if (context.Metadata.ModelType.IsExtendableEnum())
             {
                 return new ExtendableEnumBinder();
             }
